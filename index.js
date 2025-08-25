2d235ad506ceb8d39354bbb69c861c401262e159
ajprod5555 <ajprod5555@gmail.com>
2025-08-25 17:09:12 -0400

    Edit .github/workflows/main.yml, apply.yml, index.js; little edits

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('[ .. ..2 GHA GHCR ETC .. ]');
+  res.write('[ .. ..2 !!!!! 0.0.2 ..2 GHA GHCR ETC .. ]');
   res.end();
 }).listen(8080);
